0f2449de37877641a3f4abc8d40fe2e4319b559d
camiladacol <camila.dacol@correo.ucu.edu.uy>
2026-06-14 10:59:01 -0300

    Arreglo nombre

--- NewProyectBBDD/Backend/ABM/__init__.py
+++ NewProyectBBDD/Backend/ABM/__init__.py
@@ -2,13 +2,13 @@ from flask import Blueprint
 
 abm_bp = Blueprint('abm_master', __name__)
 
-from .estudiante import estudiante_bp
+from .estudiantes import estudiantes_bp
 from .disciplinas import disciplinas_bp
 from .espacios import espacios_bp
 from .actividades import actividades_bp
 from .asistencias import asistencias_bp
 
-abm_bp.register_blueprint(estudiante_bp)
+abm_bp.register_blueprint(estudiantes_bp)
 abm_bp.register_blueprint(disciplinas_bp)
 abm_bp.register_blueprint(espacios_bp)
 abm_bp.register_blueprint(actividades_bp)
